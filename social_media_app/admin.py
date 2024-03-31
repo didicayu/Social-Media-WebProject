b9caedcfedbad02394c37b0c57517d123b50fb1b
Dídac Cayuela <42339480+didicayu@users.noreply.github.com>
2024-03-31 23:58:37 +0200

    main commit

--- social_media_app/admin.py
+++ social_media_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import BrandCompany, ProductService, SocialMediaUser, SocialMediaPost, UserInteraction
+from .models import BrandCompany, ProductService, SocialMediaUser, SocialMediaPost, UserInteraction
 
 # Register your models here.
 admin.site.register(BrandCompany)
